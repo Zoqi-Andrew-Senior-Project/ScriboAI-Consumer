1a5d53676f74f60b82be36f1513c31718494ca5a
Andrew <martinezjandrew@gmail.com>
2024-10-10 17:13:14 -0500

    Edited App.js

--- consumer_frontend/consumer-frontend/src/App.js
+++ consumer_frontend/consumer-frontend/src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This will be replaced by a new home page!!!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
