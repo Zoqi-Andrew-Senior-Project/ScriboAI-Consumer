08f550e8d6c782daa28117f9b082ee8e8f170a33
Andrew <martinezjandrew@gmail.com>
2024-12-03 15:27:41 -0600

    fixed dynamically updating outline and data to send to DB

--- consumer_frontend/src/loadCourseOutline.js
+++ consumer_frontend/src/loadCourseOutline.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import './courseoutline.css'
 
-const EditableLine = ({ text, onSave, type = "text" }) => {
+const EditableLine = ({ text, onSave, type = "text", editable = false }) => {
     const [isEditing, setIsEditing] = useState(false);
     const [newText, setNewText] = useState(text);
 
     const handleDoubleClick = () => {
-        setIsEditing(true);
+        if (editable) {
+            setIsEditing(true);
+        }
     };
 
     const handleBlur = () => {
@@ -28,7 +30,7 @@ const EditableLine = ({ text, onSave, type = "text" }) => {
 
     return (
         <div className="editable-line" onDoubleClick={handleDoubleClick}>
-        {isEditing ? (
+        {isEditing  && editable ? (
                 type === "text" ? (
                     <input
                         type="text"
@@ -56,7 +58,7 @@ const EditableLine = ({ text, onSave, type = "text" }) => {
     
 const Feature = ({ feature }) => <li>{feature}</li>
 
-const Module = ({ module, updateModule }) => {
+const Module = ({ module, updateModule, editable }) => {
     const handleSubtopicChange = (index, newText) => {
         const updatedSubtopics = [...module.subtopics]
         updatedSubtopics[index] = newText
@@ -65,16 +67,28 @@ const Module = ({ module, updateModule }) => {
 
     return (
         <div className="module">
-            <h4>Title:<EditableLine text={module.name} onSave={(newName) =>
-                updateModule({ ...module, name: newName })
-            }/></h4>
+            <h4>Title: {" " }
+                <EditableLine 
+                    text={module.name} 
+                    onSave={(newName) =>
+                    updateModule({ ...module, name: newName })
+                    }
+                    editable={editable}
+                />
+            </h4>
             <h4>Duration: {module.duration}</h4>
             <ul>
                 <h5>Subtopics:</h5>
                 {module.subtopics.map((subtopic, index) => (
-                    <li key={index}><EditableLine text={subtopic} onSave={(newText) =>
-                        handleSubtopicChange(index, newText)
-                    }/></li>
+                    <li key={index}>
+                        <EditableLine 
+                            text={subtopic} 
+                            onSave={(newText) =>
+                                handleSubtopicChange(index, newText)
+                            }
+                            editable={editable}
+                        />
+                    </li>
                 ))}
             </ul>
             <ul>
@@ -87,52 +101,64 @@ const Module = ({ module, updateModule }) => {
     )
 }
 
-const CourseOutlineRenderer = ({ data }) => {
-    const [courseData, setCourseData] = useState(data)
-
-    const handleSave = (newData) => {
-        console.log("data:", data);
-        console.log("courseData:", courseData)
-        // Logic to update the data.title can go here
-    };
-
+const CourseOutlineRenderer = ({ editable = false, courseOutlineData, setCourseOutlineData }) => {
     const updateModule = (index, updatedModule) => {
-        const updatedModules = [...courseData.modules]
+        const updatedModules = [...courseOutlineData.modules]
         updatedModules[index] = updatedModule
-        setCourseData({ ...courseData, modules: updatedModules })
+        setCourseOutlineData({ ...courseOutlineData, modules: updatedModules })
     }
     
     return (
         <div class="course-outline" id="course outline">
-            <h1>Title: <EditableLine text={data.title} onSave={(newTitle) => 
-                setCourseData({ ...courseData, title: newTitle})
-            }/></h1>
-            <h4>Duration: {data.duration}</h4>
+            <h1>Title: {" "}
+                <EditableLine 
+                    text={courseOutlineData.title} 
+                    onSave={(newTitle) => 
+                        setCourseOutlineData({ ...courseOutlineData, title: newTitle})
+                    }
+                    editable={editable}
+                />
+            </h1>
+            <h4>Duration: {courseOutlineData.duration}</h4>
             <h3>Summary:</h3>
-            <EditableLine text={data.summary} onSave={(newSummary) => 
-                setCourseData({ ...courseData, summary: newSummary})
-            } type="textbox"/>
+            <EditableLine 
+                text={courseOutlineData.summary} 
+                onSave={(newSummary) => 
+                    setCourseOutlineData({ ...courseOutlineData, summary: newSummary})
+                } 
+                type="textbox"
+                editable={editable}
+            />
             <h3>Objectives:</h3>
             <ul>
-                {data.objectives.map((objective, index) => (
-                <li key={index}><EditableLine text={objective} onSave={(newObjective) => {
-                    const updatedObjectives = [...courseData.objectives]
-                    updatedObjectives[index] = newObjective
-                    setCourseData({ ...courseData, objectives: updatedObjectives })
-                }
-                }/></li>
+                {courseOutlineData.objectives.map((objective, index) => (
+                <li key={index}>
+                    <EditableLine 
+                        text={objective} 
+                        onSave={
+                            (newObjective) => {
+                                const updatedObjectives = [...courseOutlineData.objectives]
+                                updatedObjectives[index] = newObjective
+                                setCourseOutlineData({ ...courseOutlineData, objectives: updatedObjectives })
+                            }
+                        }
+                        editable={editable}
+                    />
+                </li>
                 ))}
             </ul>
             <div class="module-container">
                 <h3>Modules:</h3>
-                {data.modules.map((module, index) => (
+                {courseOutlineData.modules.map((module, index) => (
                     <Module
                         key={index}
                         module={module}
                         updateModule={(updatedModule) => updateModule(index, updatedModule)}
+                        editable={editable}
                     />
                 ))}
             </div>
+            <button onClick={console.log(courseOutlineData)}>Hello</button>
         </div>
     )
 }
