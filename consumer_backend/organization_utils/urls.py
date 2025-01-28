98666b9cb87621422ec715119e37ac26e993e90c
Andrew <martinezjandrew@gmail.com>
2025-01-27 18:29:16 -0600

    removed or marked deprecated old code

--- consumer_backend/organization_utils/urls.py
+++ consumer_backend/organization_utils/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path("create-organization/", views.create_organization, name="create_organization"),
     path("delete-organization/", views.delete_organization, name="delete_organization"),
-    path("create-member/", views.create_member, name="create_member"),
     path("invite-member/", views.invite_member, name="invite_member"),
     path("join-organization/<str:invitation_token>/", views.create_member_by_token, name='join-organization'),
     path("complete-profile/", views.complete_profile, name="complete_profile"),
