8fa5f1be972ed97e07b46bb5b9416bd904aaddb2
andrew martinez <56596735+martinezjandrew@users.noreply.github.com>
2025-02-05 04:04:31 -0600

    Delete archive/consumer_backend_OLD directory
    Cleaning up the file directory

--- archive/consumer_backend_OLD/mysite/courses/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Course
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = ['id', 'prompt', 'created_at']
--- archive/consumer_backend_OLD/mysite/training_operations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("api/index", views.index.as_view(), name="index"),
-]
