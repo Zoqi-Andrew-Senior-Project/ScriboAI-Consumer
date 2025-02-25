6b20c7ce8eb6bfb0aa44d83d80d87a6249d59a2f
Andrew <martinezjandrew@gmail.com>
2025-02-25 01:42:04 -0600

    Add API calls to Member actions

--- consumer_frontend/src/AdminPages/EmployeeDashboard.js
+++ consumer_frontend/src/AdminPages/EmployeeDashboard.js
@@ -9,15 +9,17 @@ const InviteButton = () =>{
 }
 const MemberTable = () => {
     const [members, setMembers] = useState([]);
-
+    const [selectedMember, setSelectedMember] = useState(null);
     const [isMemberActionFormOpen, setIsMemberActionFormOpen] = useState(false);
 
-    const handleShowForm = () => {
+    const handleShowForm = (member) => {
+        setSelectedMember(member);
         setIsMemberActionFormOpen(true);
     }
 
     const handleCloseForm = () => {
         setIsMemberActionFormOpen(false);
+        setSelectedMember(null);
     }
 
     useEffect(() => {
@@ -53,7 +55,7 @@ const MemberTable = () => {
                             <td>{member.role}</td>
                             <td>{member.status}</td>
                             <td>
-                                <button onClick={handleShowForm} className="btn btn-secondary">
+                                <button onClick={() => handleShowForm(member)} className="btn btn-secondary">
                                     🛠️
                                 </button>
                             </td>
@@ -61,28 +63,123 @@ const MemberTable = () => {
                     ))}
                 </tbody>
             </table>
-            <MemberActionForm isOpen={isMemberActionFormOpen} onClose={handleCloseForm} />
+            <MemberActionForm isOpen={isMemberActionFormOpen} onClose={handleCloseForm} member={selectedMember}/>
         </div>
     )
 }
 
-const MemberActionForm = ({isOpen, onClose}) => {
+const MemberActionForm = ({isOpen, onClose, member}) => {
     const formRef = useRef(null);
+    const [isRoleFormOpen, setIsRoleFormOpen] = useState(false);
+    const [isDeleteFormOpen, setIsDeleteFormOpen] = useState(false);
+    const [selectedRole, setSelectedRole] = useState(null);
+    const { user, loading } = useAuth();
 
     const handleClickOutside = (e) => {
         if (!formRef.current.contains(e.target)) {
             onClose();
+            setIsRoleFormOpen(false);
+            setIsDeleteFormOpen(false);
         }
     }
 
-    if (!isOpen) return null;
+    const handleDelete = () => {
+        axios
+          .delete(process.env.REACT_APP_BACKEND_ADDRESS + "/api/org/member", {
+            withCredentials: true,  // Ensures cookies are sent
+            data: {
+                member_username: member.user_name
+            }
+          })
+          .then((response) => {
+              console.log(response.data);
+              onClose();
+              setIsRoleFormOpen(false);
+              setIsDeleteFormOpen(false);
+          })
+          .catch((error) => console.error("Error deleting member:", error));
+    }
+
+    const handleClose = () => {
+        onClose();
+        setIsRoleFormOpen(false);
+        setIsDeleteFormOpen(false);
+    }
+
+    const handleRoleChange = (newRole) => {
+        if (newRole !== member.role) {
+            setSelectedRole(newRole);
+
+            axios.put(process.env.REACT_APP_BACKEND_ADDRESS + "/api/org/member/", {
+                member_username: member.user_name,
+                role: newRole
+            }, {
+                withCredentials: true,  // Ensures cookies are sent
+            })
+            .then((response) => {
+                console.log(response.data);
+                onClose();
+                setIsRoleFormOpen(false);
+                setIsDeleteFormOpen(false);
+            })
+        } else {
+            console.log("Role has not changed");
+        }
+    }
+
+    const openRoleForm = () => {
+        setIsRoleFormOpen(true);
+    }
+
+    const closeRoleForm = () => {
+        setIsRoleFormOpen(false);
+    }
+
+    const openDeleteForm = () => {
+        setIsDeleteFormOpen(true);
+    }
+
+    const closeDeleteForm = () => {
+        setIsDeleteFormOpen(false);
+    }
+
+    if (!isOpen || !member) return null;
+
     return (
         <div className='overlay' onClick={handleClickOutside}>
             <div className='form-container' ref={formRef}>
-                <button className="close-button" onClick={onClose}>&times;</button>
+                <button className="close-button" onClick={handleClose}>&times;</button>
                 <h1>Member Actions</h1>
-                <div> 🔁 Change Role </div>
-                <div> 🗑️ Remove </div>
+                <p>{member.user_name}</p>
+                <div>
+                    <button className='btn btn-tertiary' onClick={openRoleForm}> 🔁 Change Role </button>
+                </div>
+                <div>
+                    <button className='btn btn-tertiary' onClick={openDeleteForm}> 🗑️ Remove </button>
+                </div>
+                {isRoleFormOpen && (
+                    <div className='form-container'>
+                        <h2>Change Role</h2>
+                        {(member.role !== 'AD'  && member.role !== 'OW' && user.role === 'OW') && (
+                            <button className='btn btn-tertiary' onClick={() => handleRoleChange('AD')}>TO ADMIN</button>
+                        )}
+                        {(member.role !== 'EM' && member.role !== 'OW' && user.role === 'OW') && (
+                            <button className='btn btn-tertiary' onClick={() => handleRoleChange('EM')}>TO EMPLOYEE</button>
+                        )}
+                        {member.role === 'OW' && (
+                            <p>No Actions Available.</p>
+                        )}
+                        <button className='btn btn-tertiary' onClick={closeRoleForm}>Cancel</button>
+                    </div>
+                )}
+                {isDeleteFormOpen && (
+                    <div className='form-container'>
+                        <h2>Remove Member</h2>
+                        <p>Are you sure you want to remove {member.user_name} from the organization?</p>
+                        <button className='btn btn-tertiary' onClick={closeDeleteForm}>Cancel</button>
+                        <button className='btn btn-tertiary' onClick={handleDelete}>Remove</button>
+                    </div>
+                )}
             </div>
         </div>
     )
