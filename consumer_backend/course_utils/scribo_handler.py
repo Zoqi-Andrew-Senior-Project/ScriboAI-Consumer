669ff37b3ef2973ab8a89675ce906ec5105f30a5
Andrew <martinezjandrew@gmail.com>
2025-03-12 15:40:25 -0500

    Add class to handle api calls to Scribo

---
+++ consumer_backend/course_utils/scribo_handler.py
@@ -0,0 +1,48 @@
+import os
+import requests
+import json
+
+class ScriboHandler():
+    def __init__(self):
+        self.api_address = os.environ.get('REACT_APP_HUGGINGFACE_ADDRESS')
+
+    def generate_course_outline(self, data):
+        if data.get('topic') is None:
+            raise Exception('No topic provided')
+        if data.get('time') is None:
+            raise Exception('No duration provided')
+        
+        url = self.api_address + '/generate-outline'
+        
+        response = requests.post(url, json=data)
+
+        data = response.json()
+
+        course_outline = data.get("response").get("output_validator").get("valid_replies")
+
+        return json.loads(course_outline)
+    
+    def update_course_outline(self, data):
+        """Body:
+
+        {
+            script: string,
+            comments: string
+        }"""
+        if data.get('script') is None:
+            raise Exception('No script provided.')
+        if data.get('notes') is None:
+            raise Exception('No notes are provided')
+        
+        url = self.api_address + '/update-outline'
+
+        print("inside update course outline")
+        print(data)
+        
+        response = requests.post(url, json=data)
+
+        data = response.json()
+
+        course_outline = data.get("response").get("output_validator").get("valid_replies")
+
+        return json.loads(course_outline)
