72aa6499851f35ee80749b672588bcb9e4955a27
Andrew <martinezjandrew@gmail.com>
2025-03-20 00:53:49 -0500

    Create WS Connection to handle Page manipulation

--- consumer_backend/course_utils/consumers.py
+++ consumer_backend/course_utils/consumers.py
@@ -1,8 +1,9 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .scribo_handler import ScriboHandler
-from .serializers import CourseWithModulesSerializer
-from .models import Course
+from .serializers import CourseWithModulesSerializer, PageSerializer
+from .models import Course, Module
+import markdown
 
 class DocumentConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,24 +14,61 @@ class DocumentConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
+        uuid = self.scope['url_route']['kwargs']['doc_id']
+
+        module = Module.objects.get(uuid=uuid)
+
+        page_serializer = PageSerializer({"currentPage": module.uuid})
+
+        message = {
+            "status": "good",
+            "data": page_serializer.data
+        }
+
+        await self.send(text_data=json.dumps(message))
+
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         content = data["content"]
+        response_data = data
+
+        action = data.get("action", None)
+
+        if action == "next":
+            nextPage = data.get("nextPage", None)
+
+            if nextPage:
+                page_serializer = PageSerializer({"currentPage": nextPage})
+                response_data = page_serializer.data
+            
 
         # Broadcast changes to all users in the room
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "document_update",
-                "content": content
+                "status": "good",
+                "data": response_data
             }
         )
 
     async def document_update(self, event):
-        await self.send(text_data=json.dumps({"content": event["content"]}))
+        message = {
+            "status": event["status"],
+            "data": event["data"]
+        }
+        await self.send(text_data=json.dumps(message))
+
+class DocumentActions():
+    def __init__(self):
+        self.scribo = ScriboHandler()
+    def generate(self):
+        md = markdown.Markdown(extensions=["fenced_code"])
+        content = md.convert(self.scribo.generate_page())
+        return content
 
 class OutlineConsumer(AsyncWebsocketConsumer):
     async def connect(self):
--- consumer_backend/course_utils/routing.py
+++ consumer_backend/course_utils/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import DocumentConsumer, OutlineConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/document/(?P<doc_id>\w+)/$", DocumentConsumer.as_asgi()),
+    re_path(r"ws/document/(?P<doc_id>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})/$", DocumentConsumer.as_asgi()),
     re_path(r"ws/outline/(?P<cor_uuid>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})/$", OutlineConsumer.as_asgi()),]
