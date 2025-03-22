0007ac1109c9ac1afed6c515c3521ba55fe248e7
Andrew <martinezjandrew@gmail.com>
2025-03-21 21:12:29 -0500

    Update RESTful API endpoints for Course

--- consumer_backend/course_utils/urls.py
+++ consumer_backend/course_utils/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
-    path("create-outline/", views.create_course_outline, name="create_course_outline"),
-    path("get-course/", views.get_course, name="get_course"),
+    path("course/", CourseView.as_view(), name="course"),
     path("get-page/", views.get_page, name="get_page"),
     path("initialize-pages/", views.initialize_pages, name="initialize_pages")
 ]
--- consumer_backend/course_utils/views.py
+++ consumer_backend/course_utils/views.py
@@ -7,55 +7,77 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 import json
 from .scribo_handler import ScriboHandler
+from organization_utils.models import Member, Organization
+from rest_framework.permissions import IsAuthenticated
+from organization_utils.permissions import IsOwnerOrAdmin
+from rest_framework.views import APIView
 
-"""
-@swagger_auto_schema(
-        method='post',
-        operation_summary="Create a course outline.",
-        operation_description="Endpoint to create a new course outline.",
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            required=['name'],
-            properties={
-                'title': openapi.Schema(type=openapi.TYPE_STRING, description="The title of the course"),
-                'objectives': openapi.Schema(type=openapi.TYPE_STRING, description="The lesson objectives for the course"),
-                'duration': openapi.Schema(type=openapi.TYPE_STRING, description="The desired estimated time duration of the course"),
-                'modules': openapi.Schema(type=openapi.TYPE_STRING, description="Modules that the course is made of"),
-                'summary': openapi.Schema(type=openapi.TYPE_STRING, description="The summary of the course"),
-                'organization': openapi.Schema(type=openapi.TYPE_STRING, description="The organization the course belongs too"),
-        },
-    ),
-    responses={
-        201: "Organization created successfully.",
-        400: "Invalid request data.",
-    },
-)
-"""
-@api_view(["POST"])
-def create_course_outline(request):
-    """
-    Create a course outline.
-    """
-    scribo = ScriboHandler()
-
-    course_outline = scribo.generate_course_outline(request.data)
-
-    course_serializer = CourseWithModulesSerializer(data=course_outline)
-
-    if course_serializer.is_valid():
-        course = course_serializer.save()
-
-        return Response({"uuid": course.uuid}, status=status.HTTP_201_CREATED)
+class CourseView(APIView):
+    def get_permissions(self):
+        if self.request.method in ['DELETE', 'POST']:
+            permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+        elif self.request.method in ['GET']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]
     
-    return Response(course_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    """
+    @swagger_auto_schema(
+            method='post',
+            operation_summary="Create a course outline.",
+            operation_description="Endpoint to create a new course outline.",
+            request_body=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                required=['name'],
+                properties={
+                    'title': openapi.Schema(type=openapi.TYPE_STRING, description="The title of the course"),
+                    'objectives': openapi.Schema(type=openapi.TYPE_STRING, description="The lesson objectives for the course"),
+                    'duration': openapi.Schema(type=openapi.TYPE_STRING, description="The desired estimated time duration of the course"),
+                    'modules': openapi.Schema(type=openapi.TYPE_STRING, description="Modules that the course is made of"),
+                    'summary': openapi.Schema(type=openapi.TYPE_STRING, description="The summary of the course"),
+                    'organization': openapi.Schema(type=openapi.TYPE_STRING, description="The organization the course belongs too"),
+            },
+        ),
+        responses={
+            201: "Organization created successfully.",
+            400: "Invalid request data.",
+        },
+    )
+    """
+    def post(self, request):
+        """
+        Create a course outline.
+        """
 
-@api_view(["GET"])
-def get_course(request):
-    course = Course.objects.get(uuid=request.data['uuid'])
+        try:
+            user = request.user
 
-    course_serializer = CourseWithModulesSerializer(course)
+            member: Member = Member.objects.get(user_name=user.username)
 
-    return Response({"course": course_serializer.data}, status=status.HTTP_200_OK)
+            scribo = ScriboHandler()
+
+            course_outline = scribo.generate_course_outline(request.data)
+
+            course_outline["organization"] = member.organization.uuid
+
+            course_serializer = CourseWithModulesSerializer(data=course_outline)
+
+            if course_serializer.is_valid():
+                course = course_serializer.save()
+
+                return Response({"uuid": course.uuid}, status=status.HTTP_201_CREATED)
+            
+            return Response(course_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Member.DoesNotExist:
+            return Response("member doesnt exist...", status=status.HTTP_404_NOT_FOUND)
+        
+    def get(self, request):
+        course = Course.objects.get(uuid=request.data['uuid'])
+
+        course_serializer = CourseWithModulesSerializer(course)
+
+        return Response({"course": course_serializer.data}, status=status.HTTP_200_OK)
 
 @api_view(["POST"])
 def initialize_pages(request):
